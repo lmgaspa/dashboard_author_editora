cc50cc21bd0c03ce952f69274fcceacb1d54bf30
lmgaspa <luhmgasparetto@gmail.com>
2026-01-01 02:42:08 -0300

    ticket

--- backend/src/main/java/com/dianaglobal/paineldoauthorbackend/domain/model/TicketCategory.java
+++ backend/src/main/java/com/dianaglobal/paineldoauthorbackend/domain/model/TicketCategory.java
@@ -4,11 +4,9 @@ package com.dianaglobal.paineldoauthorbackend.domain.model;
  * Categoria de um ticket.
  */
 public enum TicketCategory {
-    PAGAMENTO,  // Relacionado a pagamentos/cobranças
-    TECNICO,    // Problemas técnicos
-    DUVIDA,     // Dúvidas gerais
-    OUTRO       // Outros assuntos
+    PAGAMENTO, // Relacionado a pagamentos/cobranças
+    TECNICO, // Problemas técnicos
+    ALTERACAO, // Alteração de dados cadastrais/bancários
+    DUVIDA, // Dúvidas gerais
+    OUTRO // Outros assuntos
 }
-
-
-
