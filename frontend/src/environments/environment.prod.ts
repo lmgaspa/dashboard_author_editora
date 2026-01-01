b5c0b4e81953ea3076224067da0fdc0211740f2a
lmgaspa <luhmgasparetto@gmail.com>
2026-01-01 05:00:43 -0300

    backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,8 @@ export const environment = {
   apiUrl: 'https://dashboard-painel-autores-vl-26b242c59563.herokuapp.com',
   backendPort: 8000,
   ga4MeasurementId: 'YOUR_GA4_ID', // TODO: Replace with actual ID
-  lookerStudioUrl: 'YOUR_LOOKER_URL' // TODO: Replace with actual URL
+  lookerStudioUrl: 'YOUR_LOOKER_URL', // TODO: Replace with actual URL
+  efiPayeeCode: 'b348a05b9c0391c8097ab315eb3b4d56',
+  efiSandbox: false
 };
 
